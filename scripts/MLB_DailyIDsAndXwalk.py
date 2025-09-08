16dde16a2d17598b064a1f52e15c56f4eddad286
cpenn16 <cpennell16@gmail.com>
2025-09-08 09:27:12 -0400

    update mlb

--- scripts/MLB_DailyIDsAndXwalk.py
+++ scripts/MLB_DailyIDsAndXwalk.py
@@ -28,7 +28,7 @@ from difflib import SequenceMatcher
 THIS = Path(__file__).resolve()
 ROOT = THIS.parents[1]  # repo root (should contain /public)
 
-DEFAULT_XLSM   = r"C:\Users\cpenn\Dropbox\Sports Models\MLB\MLB September 5th.xlsm"
+DEFAULT_XLSM   = r"C:\Users\cpenn\Dropbox\Sports Models\MLB\MLB September 8th.xlsm"
 DEFAULT_PROJ   = str(ROOT)
 DEFAULT_CONFIG = str(ROOT / "scripts" / "configs" / "mlb_classic.json")
 
