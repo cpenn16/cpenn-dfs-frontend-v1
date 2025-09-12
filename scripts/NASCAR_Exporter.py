cab16db4d9f12b5ade8be99fe46d0c83672b4dbe
cpenn16 <cpennell16@gmail.com>
2025-09-12 15:31:39 -0400

    update

--- scripts/NASCAR_Exporter.py
+++ scripts/NASCAR_Exporter.py
@@ -29,9 +29,9 @@ import numpy as np
 import pandas as pd
 
 # --------------------- DEFAULTS ---------------------
-DEFAULT_XLSM   = r"C:\Users\cpenn\Dropbox\Sports Models\2025 NASCAR\Trucks UNOH 200 Bristol.xlsm"
+DEFAULT_XLSM   = r"C:\Users\cpenn\Dropbox\Sports Models\2025 NASCAR\Xfinity Food City 300 Bristol.xlsm"
 DEFAULT_PROJ   = r"C:\Users\cpenn\Downloads\cpenn-dfs_frontend-v1"
-DEFAULT_CONFIG = r"C:\Users\cpenn\Downloads\cpenn-dfs_frontend-v1\scripts\configs\nascar_trucks.json"
+DEFAULT_CONFIG = r"C:\Users\cpenn\Downloads\cpenn-dfs_frontend-v1\scripts\configs\nascar_xfinity.json"
 # ---------------------------------------------------
 
 # Directories that should receive a meta.json (populated as we export)
